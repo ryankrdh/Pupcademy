feb44d59247f3260644e923a3a21bd4b7a4cbcff
ryankrdh <ryankrdh@gmail.com>
2023-07-07 15:17:54 -0700

    base.html done

---
+++ main.py
@@ -0,0 +1,6 @@
+from website import create_app # website(name of folder) becomes a python package because we have __init__.py in that folder. That's why we can import the function create_app.
+
+app = create_app()
+
+if __name__ == '__main__': # only run this file if we have it, NOT imported. You only want to run the web server if you have the '__main__' file.
+    app.run(debug=True) # anytime we make change to our python code, it will rerun the web server. **Turn this off when running the website in production**
--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,10 @@ def create_app():
     app = Flask(__name__) # initializing flask. 
     app.config['SECRET_KEY'] = 'asdf;lkj' # encrypts the cookies and session data
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
